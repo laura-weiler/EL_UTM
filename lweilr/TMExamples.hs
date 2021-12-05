3d9e94d499a13636e353246603966a36c02662e4
laura-weiler <wmlaura96@gmail.com>
2021-12-04 18:39:08 -0600

    added first set of transition functions

--- lweilr/TMExamples.hs
+++ lweilr/TMExamples.hs
@@ -20,43 +20,19 @@ tripletm =
             goRight 5 '!' '!' 1 
 
 el_utm =
-  TM [0 .. 25] "01?,#." "01?,#.ab" id ' ' '?' trans 1 [25]
+  TM [0 .. 25] "01?,#." "01?,#.ab@" id ' ' '?' trans 1 [25]
   where
     trans = goRight 0 '?' '?' 1 ++
-            ---- BEGIN WRITING THE START STATE TO THE FIRST BLANK
-            -- now traverse right 
-            loopRight 1 "01,."  ++
-            goRight 1 '#' '#' 2 ++
-            loopRight 2 "01,." ++ 
-            goRight 2 '#' '#' 3 ++
-            loopRight 3 "01,." ++
-            goRight 3 '#' '#' 4 ++
-            loopRight 4 "01,." ++
-            goRight 4 '#' '#' 5 ++
-            -- at this point, should be at the start of the input string
-            -- we put a blank here to write the start state
-            goRight 5 '0' '0' 6 ++
-            goLeft 6 '0' 'a' 7 ++
-            loopLeft 7 "01,." ++
-            goLeft 7 '#' '#' 8 ++
-            loopLeft 8 "01,." ++
-            goLeft 8 '#' '#' 9 ++
-            loopLeft 9 "01,." ++
-            goLeft 9 '#' '#' 10 ++
-            -- now the start state is immediately before the tape head. 
-            -- go back to the start symbol for the UTM
-            -- so that the start state is immediately after the tape head.
-            loopLeft 10 "01,." ++
-            goRight 10 '?' '?' 12 ++
-            goRight 10 'a' '0' 12 ++
-            goRight 10 'b' '1' 12 ++
-            goRight 12 '0' 'a' 13 ++
-            -- now go back to write the start state to the blank we're processing
-            -- case for a
-            loopRight 13 "01,#." ++
-            goRight 13 'a' '0' 14 ++
-            -- case for 1,b
-            goRight 12 '1' 'b' 15 ++
-            loopRight 15 "01,#." ++
-            goRight 15 'a' '1' 5
-            ---- END WRITING THE START STATE TO THE FIRST BLANK
+            ---- BEGIN WRITING THE START STATE TO THE LEFT OF THE FIRST INPUT BIT
+            goRight 1 '0' 'a' 2 ++ -- case where encoded input state starts with 0
+            goRight 1 '1' 'b' 3 ++ -- case where encoded input state starts with 1
+            goRight 1 '#' '#' 25 ++
+            loopRight 2 "01,.#" ++ -- loop over everything to the start of the input string
+            loopRight 3 "01,.#" ++
+            goRight 2 '@' '0' 4 ++ -- mark that we are processing the first placeholder character
+            goRight 3 '@' '1' 4 ++
+            loopLeft 4 "01,.a#" ++ -- go back to the beginning of the tape
+            goRight 4 '?' '?' 5 ++
+            loopRight 5 "01" ++    -- find the bit we are currently processing
+            goRight 5 'a' '0' 1 ++
+            goRight 5 'b' '1' 1
